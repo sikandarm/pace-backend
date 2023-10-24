adc3f0241851228d9d068dd1b85e9755bf60abdd
hilal13339 <hilalahmad13339gmail.com>
2023-10-24 16:53:35 +0500

    first commit

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase.json"); // Path to your downloaded service account JSON file
+// const serviceAccount = require("../firebase.json"); // Path to your downloaded service account JSON file
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // Add other initialization options as required
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   // Add other initialization options as required
+// });
 
 // Export the initialized admin object
 module.exports = admin;
