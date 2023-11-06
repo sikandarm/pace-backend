2e7589da9a8cf9ceda136856159f0cfb70af5c2b
Wasif <wasifmian4@gmail.com>
2023-11-06 13:38:46 +0500

    api

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,12 +1,12 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../firebase.json"); // Path to your downloaded service account JSON file
+// const admin = require("firebase-admin");
+// const serviceAccount = require("../firebase.json"); // Path to your downloaded service account JSON file
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // Add other initialization options as required
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   // Add other initialization options as required
+// });
 
-// Export the initialized admin object
-module.exports = admin;
+// // Export the initialized admin object
+// module.exports = admin;
 
 
